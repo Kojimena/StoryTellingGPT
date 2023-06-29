3ebc164dcd651efc7ab1b2865ed780d77cdfe0a1
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2023-06-29 07:55:29 -0600

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,47 @@ import React, { useState } from "react"
 import axios from "axios"
 import StoryForm from "./components/StoryForm/StoryForm"
 import StoryDisplay from "./components/StoryDisplay/StoryDisplay"
+import "./App.css"
 
 function App() {
   const [storyInputs, setStoryInputs] = useState(null)
   const [story, setStory] = useState("")
+  const [loading, setLoading] = useState(false) // <-- Nueva variable de estado
+
+
+  const API_KEY = "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" // reemplaza esto con tu clave API real
 
   const generateStory = async (inputs) => {
+    setLoading(true) 
     console.log("Generando")
+    console.log(inputs)
     try {
       const response = await axios.post(
-        "https://api.openai.com/v1/engines/davinci/completions",
+        "https://api.openai.com/v1/chat/completions",
         {
-          prompt: `escribe una historia sobre un personaje llamado ${inputs.characterName}, de tipo ${inputs.storyType}.`,
-          max_tokens: 100,
-          temperature: 0.2,
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "system",
+              content: `Escribe una historia que empiece con la palabra Érase una vez de ${inputs.storyLength} palabras sobre los siguientes personajes: ${inputs.characterNames.join(", ")}. El tipo de historia es ${inputs.storyType}.`
+            }
+          ]
         },
         {
           headers: {
             "Content-Type": "application/json",
-            "Authorization": "Bearer ${REACT_APP_OPENAI_API_KEY}",
+            "Authorization": `Bearer ${API_KEY}`,
           },
         }
       )
       console.log(response.data)
-      setStory(response.data.choices[0].text)
+      setStory(response.data.choices[0].message.content)
     } catch (error) {
       console.error("Hubo un error al generar la historia:", error)
+    }finally {
+      setLoading(false) 
     }
   }
-  
 
   const handleStoryInputs = (inputs) => {
     setStoryInputs(inputs)
@@ -39,8 +51,21 @@ function App() {
 
   return (
     <div className="App">
+      <div className="books-container">
+        <img src="/images/books.svg" alt="Story AI" className="books" />
+      </div>
       <StoryForm setStoryInputs={handleStoryInputs} />
-      <StoryDisplay story={story} />
+      {
+        loading ? 
+        <div className="loader">
+        <div className="lds-ring"><div></div><div></div><div></div><div></div></div> 
+        </div>
+        :
+        storyInputs && storyInputs.storyType && storyInputs.numCharacters && storyInputs.storyLength && storyInputs.characterNames[0] !== "" && storyInputs.characterNames.length === parseInt(storyInputs.numCharacters) ?
+        <StoryDisplay story={story} />
+        :
+        null
+      }
     </div>
   )
 }
--- src/components/StoryForm/StoryForm.jsx
+++ src/components/StoryForm/StoryForm.jsx
@@ -1,32 +1,75 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import "./StoryForm.css"
 
 const StoryForm = ({ setStoryInputs }) => {
-  const [characterName, setCharacterName] = useState("")
   const [storyType, setStoryType] = useState("")
+  const [numCharacters, setNumCharacters] = useState("1")
+  const [storyLength, setStoryLength] = useState("")
+  const [characterNames, setCharacterNames] = useState([""]) // Almacenar los nombres de los personajes en un array
+
+  const handleCharacterNameChange = (index, event) => {
+    const newCharacterNames = [...characterNames]
+    newCharacterNames[index] = event.target.value
+    setCharacterNames(newCharacterNames)
+  }
+
+  useEffect(() => {
+    if (numCharacters === "" || isNaN(numCharacters)) {
+      return
+    }else if (parseInt(numCharacters) < 0) {
+      setNumCharacters("0")
+      return
+    }
+    const newCharacterNames = new Array(parseInt(numCharacters)).fill("")
+    setCharacterNames(newCharacterNames)
+    console.log("Cambiando el número de personajes")
+  }, [numCharacters])
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    setStoryInputs({ characterName, storyType })
+    if (storyType === "" || numCharacters === "" || storyLength === "" || characterNames.includes("")) {
+      alert("Por favor, completa todos los campos.")
+      return
+    }
+    setStoryInputs({ characterNames, storyType, numCharacters, storyLength })
   }
 
   return (
     <form className="story-form" onSubmit={handleSubmit}>
+      <h2 className="story-form-title">STORY AI</h2>
+      <h4>Número de personajes</h4>
       <input
-        type="text"
-        placeholder="Nombre del personaje"
-        value={characterName}
-        onChange={(e) => setCharacterName(e.target.value)}
+        type="number"
+        placeholder="Número de personajes"
+        value={numCharacters}
+        onChange={(e) => setNumCharacters(e.target.value)}
       />
+      <h4>Nombres de los personajes</h4>
+      {characterNames.map((name, index) => (
+        <input
+          key={index}
+          type="text"
+          placeholder={`Nombre del personaje ${index + 1}`}
+          value={name}
+          onChange={(e) => handleCharacterNameChange(index, e)}
+        />
+      ))}
       <select value={storyType} onChange={(e) => setStoryType(e.target.value)}>
-        <option value="">Selecciona el tipo de cuento</option>
+        <option value="">Tipo de cuento</option>
         <option value="aventura">Aventura</option>
         <option value="fantasia">Fantasía</option>
         <option value="terror">Terror</option>
         <option value="ciencia ficcion">Ciencia Ficción</option>
         <option value="romance">Romance</option>
       </select>
+      <h4>Longitud de la historia</h4>
+      <input
+        type="number"
+        placeholder="Longitud de la historia (en palabras)"
+        value={storyLength}
+        onChange={(e) => setStoryLength(e.target.value)}
+      />
       <button type="submit">Generar Cuento</button>
     </form>
   )
@@ -35,6 +78,5 @@ const StoryForm = ({ setStoryInputs }) => {
 StoryForm.propTypes = {
     setStoryInputs: PropTypes.func.isRequired,
 }
-
 
 export default StoryForm
